074d21e0c757bbbd92cc4eb355074fe79f8b5706
Matney0 <mlmoller21@amarillocollege.com>
2025-02-02 13:53:31 -0600

    Update Program.cs
    more clarifying information for the program

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -14,8 +14,9 @@ namespace Week3
             double usd, yen, euro;
 
             //Input(s)
-            Console.WriteLine("Welcome! Which currency did you want to convert? 1-USD  2-Yen  3-Euro");
-
+            //Provides user 3 different options
+            Console.WriteLine("Welcome! Which currency did you want to convert? 1-USD to Yen  2-Yen to Euro  3-Euro to USD");
+            //reading user input
             string userInput = Console.ReadLine();
 
             //Loop processing
@@ -23,8 +24,11 @@ namespace Week3
             if (userInput == "1")
             {
                 Console.WriteLine("Enter the amount of USD you want to convert Yen: ");
+                //converts and reads value since it wont likely be a whole number
                 usd = Convert.ToDouble(Console.ReadLine());
+                //value of USD in comparison to Yen
                 yen = usd * 109.23;
+                //Converted amount
                 Console.WriteLine("The amount of Yen you will get is: " + yen);
                 Console.ReadLine();
             }
@@ -32,8 +36,11 @@ namespace Week3
             else if (userInput == "2")
             {
                 Console.WriteLine("Enter the amount of Yen you want to convert to Euro: ");
+                //converts and reads value since it wont likely be a whole number
                 yen = Convert.ToDouble(Console.ReadLine());
+                //Value of Yen in comparison to Euro
                 euro = yen / 105.23;
+                //Converted amount
                 Console.WriteLine("The amount of USD you will get is: " + euro);
                 Console.ReadLine();
             }
@@ -41,8 +48,11 @@ namespace Week3
             else if (userInput == "3")
             {
                 Console.WriteLine("Enter the amount of Euro you want to convert to USD: ");
+                //converts and reads value since it wont likely be a whole number
                 euro = Convert.ToDouble(Console.ReadLine());
+                //Value of Euro in comparison to USD
                 usd = euro * 1.18;
+                //Converted amount
                 Console.WriteLine("The amount of USD you will get is: " + usd);
                 Console.ReadLine();
             }
